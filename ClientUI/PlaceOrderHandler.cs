5498cb13220c451eee9558d195f986384be3ca7a
Stefan Zueger <stefan.zueger@zuehlke.com>
2018-10-23 16:42:10 +0200

    step2

---
+++ ClientUI/PlaceOrderHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Messages.Commands;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace ClientUI
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<PlaceOrderHandler>();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            Log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
